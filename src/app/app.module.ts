450733f494ca37eaa023008d33d6d9e9fe8bf1e2
dn1023 <brunoprotz81@gmail.com>
2024-12-24 00:54:05 +0100

    feat: ecommerce pages added. sidebar updated.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { SidebarModule } from './sidebar/sidebar.module';
 import { AppComponent } from './app.component';
 
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { PagesComponent } from './pages/pages.component';
 
 @NgModule({
   imports: [
@@ -28,6 +29,7 @@ import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.compon
   declarations: [
     AppComponent,
     AdminLayoutComponent,
+    PagesComponent,
   ],
   providers: [],
   bootstrap: [AppComponent],
