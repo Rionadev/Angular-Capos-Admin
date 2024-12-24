cafd9e8b56e00affbc25e37f4e0751f874a5eb37
dn1023 <brunoprotz81@gmail.com>
2024-12-24 08:25:35 +0100

    update: ecommerce settings sliders page updated.

--- src/app/ecommerce/settings/settings.component.ts
+++ src/app/ecommerce/settings/settings.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsComponent implements OnInit {
 
+  activeTab: string = 'modules'; // Default to the 'Modules' tab
+  isAddSliderContentVisible: boolean = false; // Initially hidden for add or editing.
+
   constructor() { }
 
   ngOnInit(): void {
   }
+  
+  setActiveTab(tab: string): void {
+    this.activeTab = tab;
+  }
 
+  toggleAddSliderContent(): void {
+    this.isAddSliderContentVisible = !this.isAddSliderContentVisible; // Toggle the visibility
+  }
 }
