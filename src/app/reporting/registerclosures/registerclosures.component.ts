1471957bd0485200ea678c03ae70e2d2c8e08049
Riona IT <154750938+Rionadev@users.noreply.github.com>
2025-01-19 11:11:02 +0900

    feat: register clouser

--- src/app/reporting/registerclosures/registerclosures.component.ts
+++ src/app/reporting/registerclosures/registerclosures.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ReportingService } from 'app/api/reporting/api.service';
 import { sum } from 'chartist';
+import { rmSync } from 'fs';
 
 @Component({
   selector: 'app-registerclosures',
@@ -15,32 +16,25 @@ export class RegisterclosuresComponent implements OnInit {
   isShowdetailflag: boolean = false;
   filteredRecords: any;
 
+  sel_total_payment: any;
+  sel_total_caetory: any;
+
   row_sum: any;
   total_sum: any;
 
   sel_reg: any;
   selectedRegister: string = 'all'; // Default selection
-  records: any = [
-    { register: 'Main Register', timeOpened: '09:00 AM', timeClosed: '05:00 PM', storeCredit: 100, cashConcealedTotal: 200, cash: 150, credit: 50, debit: 0, refunds: 0, voided: 0, total: 200 },
-    { register: 'Main Register', timeOpened: '10:00 AM', timeClosed: '06:00 PM', storeCredit: 150, cashConcealedTotal: 250, cash: 200, credit: 40, debit: 10, refunds: 5, voided: 0, total: 245 },
-    { register: 'Register2', timeOpened: '09:30 AM', timeClosed: '05:30 PM', storeCredit: 80, cashConcealedTotal: 180, cash: 130, credit: 30, debit: 5, refunds: 0, voided: 0, total: 195 },
-    { register: 'Register3', timeOpened: '11:00 AM', timeClosed: '07:00 PM', storeCredit: 120, cashConcealedTotal: 220, cash: 160, credit: 50, debit: 5, refunds: 10, voided: 0, total: 215 },
-    { register: 'Register4', timeOpened: '08:30 AM', timeClosed: '04:30 PM', storeCredit: 90, cashConcealedTotal: 170, cash: 120, credit: 25, debit: 0, refunds: 0, voided: 1, total: 214 },
-    { register: 'Main Register', timeOpened: '09:15 AM', timeClosed: '05:15 PM', storeCredit: 110, cashConcealedTotal: 210, cash: 180, credit: 20, debit: 5, refunds: 0, voided: 0, total: 205 },
-    { register: 'Register2', timeOpened: '10:30 AM', timeClosed: '06:30 PM', storeCredit: 130, cashConcealedTotal: 230, cash: 190, credit: 40, debit: 0, refunds: 0, voided: 0, total: 230 },
-    { register: 'Register3', timeOpened: '11:30 AM', timeClosed: '07:30 PM', storeCredit: 70, cashConcealedTotal: 140, cash: 100, credit: 30, debit: 5, refunds: 0, voided: 0, total: 135 },
-    { register: 'Register4', timeOpened: '08:00 AM', timeClosed: '04:00 PM', storeCredit: 60, cashConcealedTotal: 120, cash: 90, credit: 20, debit: 0, refunds: 0, voided: 2, total: 110 },
-    { register: 'Main Register', timeOpened: '09:45 AM', timeClosed: '05:45 PM', storeCredit: 140, cashConcealedTotal: 240, cash: 200, credit: 30, debit: 10, refunds: 5, voided: 0, total: 235 },
-  ];
+  detail_records: any
   ngOnInit(): void {
     this.init_row_sum();
     this.init_total_sum();
+    this.fetchRegisters();
     this.fetchSearchItems();
   }
   value(index, item) {
     return item;
   }
-  fetchSearchItems() {
+  fetchRegisters() {
     //fecthRegister
     this.reportingService.fecthRegister().subscribe(
       (res) => {
@@ -62,12 +56,13 @@ export class RegisterclosuresComponent implements OnInit {
         // Handle the error as needed
       }
     );
+  }
+  fetchSearchItems() {
+
     this.reportingService.fetchPaymentReg({
       register: this.selectedRegister
     }).subscribe(
       (res) => {
-
-
         this.filteredRecords = res;
         this.filteredRecords = this.filteredRecords.map(item => ({
           ...item,
@@ -75,15 +70,19 @@ export class RegisterclosuresComponent implements OnInit {
           formattedClosingTime: this.getFormattedDate(item.closing_time),
           store_credit: this.calcCashPayment('store_credit', item.open_value),
           cash_concealed: this.calcCashPayment('cash_concealed', item.payment_data.all_payments),
-          cash: this.calcCashPayment('cash', item.payment_data.cash_movements),
+          cash_d: this.calcCashPayment('cash_d', item.payment_data.cash_movements),
+          cash: this.calcCashPayment('cash', item.payment_data.all_payments),
           credit: this.calcCashPayment('credit', item.payment_data.all_payments),
           debit: this.calcCashPayment('debit', item.payment_data.all_payments),
           refunds: this.calcCashPayment('refunds', item.payment_data.all_returns),
           voided: this.calcCashPayment('voided', item.payment_data.all_voided),
           total: this.calcSumCashPayment(),
+          // paymentSummary: this.calcCashPayment('paymentSummary', item.payment_data.all_voided),
+          // categorySummary: this.calcCashPayment('categorySummary', item.payment_data.all_voided),
+
         }));
         console.log(this.total_sum);
-        this.records = this.filteredRecords;
+        console.log(this.filteredRecords);
       },
       (error) => {
         console.error('Error fetching customer data:', error);
@@ -93,8 +92,14 @@ export class RegisterclosuresComponent implements OnInit {
   }
   calcSumCashPayment() {
     let sum = 0;
-    sum = this.row_sum['store_credit'] + this.row_sum['cash_concealed'] + this.row_sum['cash'] + this.row_sum['credit']
-      + this.row_sum['debit'] + this.row_sum['refunds'] + this.row_sum['voided'];
+    sum = this.row_sum['store_credit']
+      + this.row_sum['cash_concealed']
+      + this.row_sum['cash_d']
+      + this.row_sum['cash']
+      + this.row_sum['credit']
+      + this.row_sum['debit']
+      + this.row_sum['refunds']
+      + this.row_sum['voided'];
     this.row_sum['total'] = sum;
     this.total_sum['total'] += sum;
     // sum = cash + credit + debit + refunds + voided;
@@ -105,16 +110,16 @@ export class RegisterclosuresComponent implements OnInit {
     let sum = 0;
     this.row_sum[type] = 0;
     if (payData?.length < 1) { return sum; }
-    if (type == 'cash') {
+    if (type == 'cash_d') {
       payData.forEach(element => {
-        sum += element.transaction;
+        sum += element?.transaction;
       });
     } else if (type == 'refunds' || type == 'voided') {
       payData.forEach(element => {
         sum += element.total_paid;
       });
 
-    } else if (type == 'credit' || type == 'debit') {
+    } else if (type == 'credit' || type == 'debit' || type == 'cash') {
       payData.forEach(element => {
         if (element.payment_status == type) {
           sum += element.total_paid;
@@ -132,14 +137,172 @@ export class RegisterclosuresComponent implements OnInit {
     }
 
     this.row_sum[type] = sum;
+
     this.total_sum[type] += sum;
     return sum;
   }
+  onBackdropClick() {
+    this.isShowdetailflag = false;
+  }
 
   showDetails(record: any) {
-    // this.isShowdetailflag = true;
+    this.isShowdetailflag = true;
+    this.selectedRecord = record; // Set the clicked record as the selectedRecord
+    this.analysis_info(record);
+  }
+
+  analysis_info(record: any) {
     console.log(record);
-    // this.selectedRecord = record; // Set the clicked record as the selectedRecord
+    this.sel_total_payment = { sum: 0 };
+    this.sel_total_caetory = { sum: 0 };
+    this.detail_records = {};
+    const cashmove = record.payment_data.cash_movements;
+    const detail_info = {
+      all: this.calc_sale_info(record.payment_data.all_payments),
+      return: this.calc_sale_info(record.payment_data.all_returns),
+      voided: this.calc_sale_info(record.payment_data.all_voided),
+      cash: this.calc_cash_movement(record.payment_data.cash_movements) || 0,
+    }
+    console.log(detail_info);
+    console.log(this.sel_total_payment);
+    console.log(this.sel_total_caetory);
+    this.detail_records = detail_info;
+    console.log('------------------', this.detail_records.all.categoryinfo.sum);
+  }
+  calc_cash_movement(cashdata: any) {
+    let cash_move = 0;
+    if (cashdata.length > 0) {
+      cashdata.forEach(element => {
+        cash_move += element.transaction;
+      });
+    }
+    return cash_move;
+  }
+  calc_sale_info(sale_list: any): any {
+    let result = {
+      sel_info: {
+        total: 0,
+        subtotal: 0,
+        total_paid: 0,
+        tax: 0,
+        service_fee: 0,
+        tip: 0,
+        cash_tip: 0,
+        total_items: 0,
+        pay_info: {
+          total: 0,
+          total_paid: 0,
+        },
+      },
+      paymentinfo: {
+        sum: 0
+      },
+      categoryinfo: {
+        sum: 0
+      },
+    };
+
+    if (sale_list.length > 0) {
+      sale_list.forEach(sale_info => {
+
+
+        if (!result.sel_info.pay_info[sale_info.payment_status]) {
+          result.sel_info.pay_info[sale_info.payment_status] = {
+            penny: 0,
+            susbtotal: 0,
+            tax: 0,
+            total: 0,
+            total_paid: 0,
+            total_items: 0,
+          };
+        }
+        result.sel_info.pay_info[sale_info.payment_status].penny += sale_info.penny || 0;
+        result.sel_info.pay_info[sale_info.payment_status].susbtotal += sale_info.susbtotal || 0;
+        result.sel_info.pay_info[sale_info.payment_status].tax += sale_info.tax || 0;
+        result.sel_info.pay_info[sale_info.payment_status].total += sale_info.total || 0;
+        result.sel_info.pay_info[sale_info.payment_status].total_paid += sale_info.total_paid || 0;
+        result.sel_info.pay_info[sale_info.payment_status].total_items += sale_info.total_items || 0;
+
+        result.sel_info.pay_info.total += sale_info.total;
+        result.sel_info.pay_info.total_paid += sale_info.total_paid;
+
+
+        result.sel_info.total += sale_info.total || 0;
+        result.sel_info.subtotal += sale_info.subtotal || 0;
+        result.sel_info.total_paid += sale_info.total_paid || 0;
+        result.sel_info.tax += sale_info.tax || 0;
+        result.sel_info.service_fee += sale_info.service_fee || 0;
+        result.sel_info.tip += sale_info.tip || 0;
+        result.sel_info.cash_tip += sale_info.cash_tip || 0;
+        result.sel_info.total_items += sale_info.total_items || 0;
+
+
+        // Uncomment and complete this section if needed
+        if (sale_info.payments.length > 0) {
+          sale_info.payments.forEach(payment => {
+
+            if (!this.sel_total_payment[payment.type]) {
+              this.sel_total_payment[payment.type] = 0;
+            }
+            this.sel_total_payment[payment.type] += payment.amount;
+            this.sel_total_payment.sum += payment.amount;
+
+
+            // Initialize the payment type if it doesn't exist
+            if (!result.paymentinfo[payment.type]) {
+              result.paymentinfo[payment.type] = 0;
+            }
+            // Aggregate payment amounts
+            result.paymentinfo[payment.type] += payment.amount || 0;
+            result.paymentinfo.sum += payment.amount || 0
+          });
+        }
+        if (sale_info.products.length > 0) {
+          sale_info.products.forEach(product => {
+
+            if (!this.sel_total_caetory[product._id]) {
+              this.sel_total_caetory[product._id] = {
+                discount: product.discount,
+                price: product.price,
+                product_name: product.product_name,
+                tax: product.tax,
+                qty: 0,
+              }
+            }
+            this.sel_total_caetory[product._id].qty += product.qty || 0;
+            let discount_v = 1;
+            if (product.discount.value > 0) {
+              if (product.discount.mode == 'percent') {
+                discount_v = product.value / 100;
+              }
+            }
+            this.sel_total_caetory.sum += product.qty * product.price * discount_v || 0;
+
+            if (!result.categoryinfo[product._id]) {
+              result.categoryinfo[product._id] = {
+                discount: product.discount,
+                price: product.price,
+                product_name: product.product_name,
+                tax: product.tax,
+                qty: 0,
+              }
+            }
+            result.categoryinfo[product._id].qty += product.qty || 0;
+            // let discount_v = 1;
+            // if (product.discount.value > 0) {
+            //   if (product.discount.mode == 'percent') {
+            //     discount_v = product.value / 100;
+            //   }
+            // }
+            result.categoryinfo.sum += product.qty * product.price * discount_v || 0;
+          });
+        }
+
+      });
+    }
+
+    // console.log(result);
+    return result;
   }
 
   searchRecords() {
@@ -168,6 +331,7 @@ export class RegisterclosuresComponent implements OnInit {
       'store_credit': 0,
       'cash_concealed': 0,
       'cash': 0,
+      'cash_d': 0,
       'credit': 0,
       'debit': 0,
       'refunds': 0,
@@ -181,6 +345,7 @@ export class RegisterclosuresComponent implements OnInit {
       'store_credit': 0,
       'cash_concealed': 0,
       'cash': 0,
+      'cash_d': 0,
       'credit': 0,
       'debit': 0,
       'refunds': 0,
